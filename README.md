08dd5f85225b98eea20301b88ab8f0c1318212fe
djokovic55 <djokovic64@gmail.com>
2025-01-28 15:16:10 +0200

    Update readme, organize main check and script.

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
 # Formal Verification of the Packet Processing System
 
+The packet processing system is fully verified with the application of the formal methods. In this project many complexity reduction techniques (Cutpoints, Blackboxing, Case-splitting, Assume-Guarantees, Inital Value Abstractions) were applied in attempt to achieve the convergence of the main end-to-end property that should exhaustively prove the packet building process. They all made a good progress in terms of better bounded proofs, but never actually resulted in a full proof.
+
+Convergence was achieved with the application of the JasperGold's SST (State Space Tunneling) method which serves to identify good candidates and hence develop new helper assertions. Helper assertion (Assume-Guarantee) reduces the state space for the tool to explore, effectively decomposing the hard problem into smaller, more manageable parts.
+
 ## Table of contents
 
-- [Introduction](#introduction)
 - [Run Project](#run-project)
 - [System Overview](#system-overview)
 - [Formal Testbench Environment](#formal-testbench-overview)
 - [State Space Tunneling](#state-space-tunneling)
 
-## Introduction
-
-The packet processing system fully verified with the application of the formal methods. 
-
 ## Run Project
-- Clone repository
-- Run make command
+1. Clone repository
+2. Run make command
+
 ## System Overview
 
 ![System Diagram](docs/system_overview.png)
 
 - The described system is an AXI4-based system operating in a single clock domain. 
 - The main purpose of the system is packet processing according to given transaction level protocol. 
-- It should build packets from incoming raw data and parse incoming packets to extract packet info and possible transmission errors.
+- It should build packets from incoming, raw data and parse incoming packets to extract packet information and possible transmission errors.
 
 ## Formal Testbench Overview
 
--- scripts/do_top.tcl
+++ scripts/do_top.tcl
@@ -21,22 +21,14 @@ set_prove_time_limit 0s
 ################################################################################
 # Elaboration
 ################################################################################
-
 # verif
-# analyze -sv09 {checker_top.sv bind_top.sv checker_data_integrity.sv checker_axi.sv checker_fair_int.sv crc_chk_calc.sv checker_di_top.sv}
 analyze -sv09 -f verif/verif.f
-# analyze -sv09 /home/aleksadj/Desktop/formal_projects/packet_processing_crc8/scripts/verif.f
 # src
-# analyze -vhdl top.vhd top_subsystem.vhd interconnect.vhd arbiter_rr.vhd int_fsm.vhd controller.vhd external_regs.vhd data_memory.vhd master_axi_cont.vhd packet_builder.vhd packet_parser.vhd 
-# analyze -vhdl regs.vhd slave_axi_cont.vhd 
-# analyze -vhdl slave_axi_lite_ex_regs_cont.vhd slave_axi_lite_regs_cont.vhd bram.vhd fifo.vhd crc_top.vhd crc8_parallel.vhd hamming_12_8.vhd hamming_check.vhd
 analyze -vhdl -f rtl/rtl.f
-
 
 elaborate -vhdl -top {top}
 clock clk
 reset reset
-
 ################################################################################
 # Apstraction variables
 ################################################################################
@@ -59,7 +51,7 @@ set inmem_iva 0
 ################################################################################
 if {$csa == 1} {
 
-  ## Create csa debug task
+	## Create csa debug task
 	task -create csa_debug -set -source_task <embedded> -copy_stopats -copy_ratings -copy_abstractions all -copy_assumes -copy {
 	<embedded>::top.chk_top.cov_pp_pb0_work 
 	<embedded>::top.chk_top.cov_pp_pb1_work  
@@ -84,22 +76,22 @@ if {$csa == 1} {
 	<embedded>::top.chk_top.ast_pp_ecc_uncorr_err
 	}
 
-################################################################################
-# Apstraction -  Stopats for Internal Registers 
-# It is important to have the same configuration on top and in registers 
-# in order to have coherent data in both checker and rtl
-################################################################################
-# PB0 task
-################################################################################
-# Start register
+	################################################################################
+	# Apstraction -  Stopats for Internal Registers 
+	# It is important to have the same configuration on top and in registers 
+	# in order to have coherent data in both checker and rtl
+	################################################################################
+	# PB0 task
+	################################################################################
+	# Start register
 	stopat subsys.system_regs.pb0_ctrl0_s
-# if pb0 is active, start should remain low because is serves as a reset signal for some registers
+	# if pb0 is active, start should remain low because is serves as a reset signal for some registers
 	assume -name asm_stopat_top_pb0_start {chk_top.pb0_busy_top = 0 |->  subsys.system_regs.pb0_ctrl0_s = 0}
-# Addr_in register
+	# Addr_in register
 	stopat subsys.system_regs.pb0_ctrl2_s
 	assume -name asm_stopat_top_pb0_addr_in {subsys.system_regs.pb0_ctrl2_s = chk_top.pb_addr_in}
 
-# Config register
+	# Config register
 	stopat subsys.system_regs.pb0_ctrl3_s
 
 	assume -name asm_stopat_top_pb0_byte_cnt {subsys.system_regs.pb0_ctrl3_s(3 downto 0) = chk_top.pb_byte_cnt}
@@ -113,22 +105,22 @@ if {$csa == 1} {
 	assume -name asm_stopat_top_pb0_sop_val {subsys.system_regs.pb0_ctrl3_s(27 downto 25) = chk_top.pb_sop_val}
 	assume -name asm_stopat_top_pb0_data_sel {subsys.system_regs.pb0_ctrl3_s(31 downto 28) = chk_top.pb_data_sel}
 
-# Addr_out register
+	# Addr_out register
 	stopat subsys.system_regs.pb0_ctrl4_s
 	assume -name asm_stopat_top_pb0_addr_out {subsys.system_regs.pb0_ctrl4_s = chk_top.pb_addr_out}
 
-################################################################################
-# PB1 task
-################################################################################
-#
-# Start register
+	################################################################################
+	# PB1 task
+	################################################################################
+	#
+	# Start register
 	stopat subsys.system_regs.pb1_ctrl0_s
 	assume -name asm_stopat_top_pb1_start {chk_top.pb1_busy_top = 0 |->  subsys.system_regs.pb1_ctrl0_s = 0}
-# Addr_in register
+	# Addr_in register
 	stopat subsys.system_regs.pb1_ctrl2_s
 	assume -name asm_stopat_top_pb1_addr_in {subsys.system_regs.pb1_ctrl2_s = chk_top.pb_addr_in}
 
-# Config register
+	# Config register
 	stopat subsys.system_regs.pb1_ctrl3_s
 	assume -name asm_stopat_top_pb1_byte_cnt {subsys.system_regs.pb1_ctrl3_s(3 downto 0) = chk_top.pb_byte_cnt}
 	assume -name asm_stopat_top_pb1_pkt_type {subsys.system_regs.pb1_ctrl3_s(7 downto 4) = chk_top.pb_pkt_type}
@@ -141,41 +133,42 @@ if {$csa == 1} {
 	assume -name asm_stopat_top_pb1_sop_val {subsys.system_regs.pb1_ctrl3_s(27 downto 25) = chk_top.pb_sop_val}
 	assume -name asm_stopat_top_pb1_data_sel {subsys.system_regs.pb1_ctrl3_s(31 downto 28) = chk_top.pb_data_sel}
 
-# Addr_out register
+	# Addr_out register
 	stopat subsys.system_regs.pb1_ctrl4_s
 	assume -name asm_stopat_top_pb1_addr_out {subsys.system_regs.pb1_ctrl4_s = chk_top.pb_addr_out}
-################################################################################
+	################################################################################
 
-################################################################################
-# PP task
-################################################################################
-# Start register
+	################################################################################
+	# PP task
+	################################################################################
+	# Start register
 	stopat subsys.system_regs.pp_ctrl0_s
 	assume -name asm_stopat_top_pp_start {chk_top.pp_busy_top = 0 |->  subsys.system_regs.pp_ctrl0_s = 0}
-# Addr_hdr register
+	# Addr_hdr register
 	stopat subsys.system_regs.pp_ctrl2_s
 	assume -name asm_stopat_top_pp_addr_hdr {subsys.system_regs.pp_ctrl2_s = chk_top.pp_addr_hdr}
 
-# Ignore ecc errs register
+	# Ignore ecc errs register
 	stopat subsys.system_regs.pp_ctrl3_s
 	assume -name asm_stopat_pp_ignore_ecc_err {subsys.system_regs.pp_ctrl3_s = chk_top.pp_ignore_ecc_err}
 } 
+
 if {$iva == 1} {
-  ## Create iva debug task
+	## Create iva debug task
 	task -create iva_debug -set -source_task <embedded> -copy_stopats -copy_ratings -copy_abstractions all -copy_assumes -copy * 
-################################################################################
-# Apstraction - IVAs for Internal Registers 
-################################################################################
-# PB0 task
-################################################################################
-# Start register
+	################################################################################
+	# Apstraction - IVAs for Internal Registers 
+	################################################################################
+	# PB0 task
+	################################################################################
+	# Start register
 	abstract -init_value subsys.system_regs.pb0_ctrl0_s
-# Addr_in register
+	# Addr_in register
 	abstract -init_value subsys.system_regs.pb0_ctrl2_s
 	assume -bound 1 -name asm_iva_pb0_addr_in {subsys.system_regs.pb0_ctrl2_s < x"13"}
 	assume -bound 1 -name asm_iva_top_pb0_addr_in {chk_top.pb0_checker_en |-> subsys.system_regs.pb0_ctrl2_s = chk_top.pb_addr_in}
 
-# Config register
+	# Config register
 	abstract -init_value subsys.system_regs.pb0_ctrl3_s
 	assume -bound 1 -name asm_iva_pb0_data_sel {subsys.system_regs.pb0_ctrl3_s(31 downto 28) < x"3" }
 
@@ -190,22 +183,22 @@ if {$iva == 1} {
 	assume -bound 1 -name asm_iva_top_pb0_sop_val {chk_top.pb0_checker_en |-> subsys.system_regs.pb0_ctrl3_s(27 downto 25) = chk_top.pb_sop_val}
 	assume -bound 1 -name asm_iva_top_pb0_data_sel {chk_top.pb0_checker_en |-> subsys.system_regs.pb0_ctrl3_s(31 downto 28) = chk_top.pb_data_sel}
 
-# Addr_out register
+	# Addr_out register
 	abstract -init_value subsys.system_regs.pb0_ctrl4_s
 	assume -bound 1 -name asm_iva_pb0_addr_out {subsys.system_regs.pb0_ctrl4_s < x"13"}
 	assume -bound 1 -name asm_iva_top_pb0_addr_out {chk_top.pb0_checker_en |-> subsys.system_regs.pb0_ctrl4_s = chk_top.pb_addr_out}
 
-################################################################################
-# PB1 task
-################################################################################
-# Start register
+	################################################################################
+	# PB1 task
+	################################################################################
+	# Start register
 	abstract -init_value subsys.system_regs.pb1_ctrl0_s
-# Addr_in register
+	# Addr_in register
 	abstract -init_value subsys.system_regs.pb1_ctrl2_s
 	assume -bound 1 -name asm_iva_pb1_addr_in {subsys.system_regs.pb1_ctrl2_s < x"13"}
 	assume -bound 1 -name asm_iva_top_pb1_addr_in {chk_top.pb1_checker_en |-> subsys.system_regs.pb1_ctrl2_s = chk_top.pb_addr_in}
 
-# Config register
+	# Config register
 	abstract -init_value subsys.system_regs.pb1_ctrl3_s
 	assume -bound 1 -name asm_iva_pb1_data_sel {subsys.system_regs.pb1_ctrl3_s(31 downto 28) < x"3" }
 	assume -bound 1 -name asm_iva_top_pb1_byte_cnt {chk_top.pb1_checker_en |-> subsys.system_regs.pb1_ctrl3_s(3 downto 0) = chk_top.pb_byte_cnt}
@@ -219,23 +212,23 @@ if {$iva == 1} {
 	assume -bound 1 -name asm_iva_top_pb1_sop_val {chk_top.pb1_checker_en |-> subsys.system_regs.pb1_ctrl3_s(27 downto 25) = chk_top.pb_sop_val}
 	assume -bound 1 -name asm_iva_top_pb1_data_sel {chk_top.pb1_checker_en |-> subsys.system_regs.pb1_ctrl3_s(31 downto 28) = chk_top.pb_data_sel}
 
-# Addr_out register
+	# Addr_out register
 	abstract -init_value subsys.system_regs.pb1_ctrl4_s
 	assume -bound 1 -name asm_iva_pb1_addr_out {subsys.system_regs.pb1_ctrl4_s < x"13"}
 	assume -bound 1 -name asm_iva_top_pb1_addr_out {chk_top.pb1_checker_en |-> subsys.system_regs.pb1_ctrl4_s = chk_top.pb_addr_out}
-################################################################################
+	################################################################################
 
-################################################################################
-# PP task
-################################################################################
-# Start register
+	################################################################################
+	# PP task
+	################################################################################
+	# Start register
 	abstract -init_value subsys.system_regs.pp_ctrl0_s
-# Addr_hdr register
+	# Addr_hdr register
 	abstract -init_value subsys.system_regs.pp_ctrl2_s
 	assume -bound 1 -name asm_iva_pp_addr_hdr {subsys.system_regs.pp_ctrl2_s < x"13"}
 	assume -bound 1 -name asm_iva_top_pp_addr_hdr {subsys.system_regs.pp_ctrl2_s = chk_top.pp_addr_hdr}
 
-# Ignore ecc errs register
+	# Ignore ecc errs register
 	abstract -init_value subsys.system_regs.pp_ctrl3_s
 	assume -bound 1 -name asm_iva_pp_ignore_ecc_err {subsys.system_regs.pp_ctrl3_s = chk_top.pp_ignore_ecc_err}
 }
